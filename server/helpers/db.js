a037648ef4433cdc8f0ea07c6ce7659ff44dd508
filsv <serge.filippovskii@gmail.com>
2022-09-29 17:00:14 +0300

    feat: add converterUbx2Usdt, delete throw

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -4,7 +4,10 @@ import {DB} from '../index.js';
 
 export const loadDB = () => {
     fs.readFile(DB_FILE_NAME, 'utf8', function (error, data) {
-        if (error) throw error;
+        if (error) {
+            console.error(error);
+            return;
+        }
 
         DB.initDbFromDile(JSON.parse(data));
     });
@@ -12,6 +15,8 @@ export const loadDB = () => {
 
 export const saveDB = data => {
     fs.writeFile(DB_FILE_NAME, JSON.stringify(data), error => {
-        if (error) throw error;
+        if (error) {
+            console.error(error);
+        }
     });
 };
