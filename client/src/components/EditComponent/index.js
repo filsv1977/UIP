9fdd442ac20140638c9e8979abfa1fa80d2829f3
Roms <roms10885@yandex.ru>
2022-10-04 18:43:36 +0300

    feature: add new UI

---
+++ client/src/components/EditComponent/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+import useEditButtonActions from '../../hooks/useEditButtonActions';
+
+function EditComponent({data, setEditRow, setRowId, taskId, rowId, onEditTask, formSubmitted}) {
+    const {editButtonActions, isEdit, filterAction} = useEditButtonActions();
+
+    const buttons =
+        isEdit && taskId === +rowId
+            ? editButtonActions.filter(btn => +btn.id !== +filterAction.OPEN)
+            : editButtonActions.filter(btn => +btn.id === +filterAction.OPEN);
+    return (
+        <div className={'d-flex justify-content-between'}>
+            {buttons.map(x => (
+                <Button
+                    disabled={+x.id === +filterAction.SAVE ? !formSubmitted : false}
+                    key={x.id}
+                    onClick={e => x.onClick(e, data, setEditRow, setRowId, onEditTask)}
+                    variant={x.variant}
+                    style={x.style}
+                    size={'sm'}
+                >
+                    {x.text}
+                </Button>
+            ))}
+        </div>
+    );
+}
+
+export default EditComponent;
