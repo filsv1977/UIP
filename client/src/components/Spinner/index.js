dc4529b58fc75862b50e9e86ad8778e16cb6a614
Roms <roms10885@yandex.ru>
2022-10-05 18:24:28 +0300

    feature: refactoring

--- client/src/components/Spinner/index.js
+++ client/src/components/Spinner/index.js
@@ -4,43 +4,39 @@ import {useTasks} from '../../Context/reducer';
 import {useEffect, useState} from 'react';
 
 function SpinnerBtn() {
-  const {
-    state: {isLoading = false}
-  } = useTasks();
+    const {
+        state: {isLoading = false}
+    } = useTasks();
 
-  let [show, setShow] = useState(false);
-  let [isEndTimer, setIsEndTimer] = useState(false);
+    let [show, setShow] = useState(false);
+    let [isEndTimer, setIsEndTimer] = useState(false);
 
-  useEffect(() => {
-    if (isLoading) {
-      setShow(true);
-      setTimeout(() => {
-        setIsEndTimer(true);
-      }, 400);
-    } else {
-      if (isEndTimer) {
-        setShow(false);
-        setIsEndTimer(false);
-      }
-    }
-  }, [isLoading, isEndTimer]);
+    useEffect(() => {
+        if (isLoading) {
+            setShow(true);
+            setTimeout(() => {
+                setIsEndTimer(true);
+            }, 400);
+        } else {
+            if (isEndTimer) {
+                setShow(false);
+                setIsEndTimer(false);
+            }
+        }
+    }, [isLoading, isEndTimer]);
 
-  return (
-    <>
-      {show && (
-        <div
-          className="position-absolute w-100 h-100 d-flex flex-column align-items-center bg-white justify-content-center">
-          {/*<div className="spinner-border" role="status">*/}
-            <Button variant="primary" disabled>
-              <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true"/>
-              Loading...
-            </Button>
-
-          {/*</div>*/}
-        </div>
-      )}
-    </>
-  );
+    return (
+        <>
+            {show && (
+                <div className="position-absolute w-100 h-100 d-flex flex-column align-items-center bg-white justify-content-center">
+                    <Button variant="primary" disabled>
+                        <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
+                        Loading...
+                    </Button>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default SpinnerBtn;
