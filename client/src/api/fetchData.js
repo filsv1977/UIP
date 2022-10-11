511275536aa954452d497154a9048eb960632a7a
Roms <roms10885@yandex.ru>
2022-10-11 10:59:51 +0300

    feature: add create Promise action type

--- client/src/api/fetchData.js
+++ client/src/api/fetchData.js
@@ -8,18 +8,18 @@ export const fetchData = async (dispatch, id, noSetLoading = true) => {
     try {
         const url = id == null ? '/tasks' : `/tasks?implemented=${id}`;
 
-        dispatch({type: actionTypes.GET_TASKS, noSetLoading});
-        await axios.get(url).then(result => {
+        dispatch({type: actionTypes.GET_TASKS.PENDING, noSetLoading});
+       await axios.get(url).then(result => {
             if (!result.data.success) throw new Error(result.data.message);
 
             dispatch({
-                type: actionTypes.GET_TASKS_SUCCESS,
+                type: actionTypes.GET_TASKS.FULFILLED,
                 payload: {data: result.data.data, activeFilterBtn: id}
             });
         });
     } catch (e) {
         dispatch({
-            type: actionTypes.GET_TASKS_FAILED,
+            type: actionTypes.GET_TASKS.REJECTED,
             payload: e.message
         });
     }
