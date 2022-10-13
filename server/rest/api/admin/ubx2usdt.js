d4cc3639f57c04a4f9fce7439aa9f25d236b44f1
filsv <serge.filippovskii@gmail.com>
2022-10-13 15:42:29 +0300

    fix: fix get ubx2usdt

--- server/rest/api/admin/ubx2usdt.js
+++ server/rest/api/admin/ubx2usdt.js
@@ -1,7 +1,14 @@
 import exchangeUbx from '../../../helpers/exchangeUbx.js';
 
 const ubx2usdt = async (req, res) => {
-    return res.json({success: true, data: {rate: process.env.HOURLY_RATE, ubx2usdt: exchangeUbx.ubx2usdt}});
+    let ubx2usdt = exchangeUbx.ubx2usdt;
+    let result;
+    if (ubx2usdt === null) {
+        result = {success: false, message: exchangeUbx.error};
+    } else {
+        result = {success: true, data: {rate: process.env.HOURLY_RATE, ubx2usdt}};
+    }
+    return res.json(result);
 };
 
 export default ubx2usdt;
