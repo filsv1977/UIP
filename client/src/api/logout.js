a32b7c5a1d36885065fde54884731a632f514cf2
filsv <serge.filippovskii@gmail.com>
2022-10-13 14:05:25 +0300

    fix: fix footer

--- client/src/api/logout.js
+++ client/src/api/logout.js
@@ -6,13 +6,13 @@ import {delAuthorizationKey} from '../utils/localStorage';
 axiosRetry(axios, {retries: 3});
 
 export const logOut = async dispatch => {
+    delAuthorizationKey();
     try {
         await axios.get('/admin/auth/logoff').then(result => {
             dispatch({
                 type: actionTypes.LOGOUT_ADMIN.FULFILLED,
                 payload: result
             });
-            delAuthorizationKey();
         });
     } catch (e) {
         dispatch({
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+            <div className="text-center">
+                <h4 className="display-4">Ubix network Improvement Proposal (UIP)</h4>
+                <p>
+                    The Ubix Improvement Proposal is a method to improve and develop the capabilities of Ubix.network.
+                </p>
+            </div>
+            <div className="text-center">
+                <h4 className="display-4">Process</h4>
+                <p>
+                    New UIPs will be introduced by the Ubix.Network team or the Ubix.Network DAO. A reward for
+                    implementing is assigned to each UIP. The rewards will be in UBX, funded by the development fund.
+                    Interested developers can apply to implement certain UIPs.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Header;
