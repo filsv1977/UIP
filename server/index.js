14fc55c66142bb255729cf51e283a3674c48a934
Roms <roms10885@yandex.ru>
2022-10-17 19:29:56 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ process.env.NODE_ENV ='production'
 
 if (process.env.NODE_ENV === 'production') {
     console.log("here")
-    app.use(express.static(__dirname + '/build'))
-    // app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
+    app.use("*",express.static(__dirname + '/build'))
     // app.use('/admin', express.static('client/build'));
     // app.use('/open', express.static('client/build'));
     // app.use('/implemented', express.static('client/build'));
