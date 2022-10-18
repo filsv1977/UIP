59d68ec3d2c870abc5c060c793bcb9364afcb62b
filsv <serge.filippovskii@gmail.com>
2022-10-18 16:17:00 +0300

    ref: refactoring rest list

--- server/rest/api/tasks/list.js
+++ server/rest/api/tasks/list.js
@@ -1,9 +1,8 @@
 import {getList} from '../../../helpers/getList.js';
-import {isAdminByHeader} from '../../../strategies/autentification.js';
 
 const list = async (req, res) => {
     let {implemented} = req.query;
-    res.json(getList(implemented, isAdminByHeader(req)));
+    res.json(getList(implemented, false));
 };
 
 export default list;
--- server/strategies/autentification.js
+++ server/strategies/autentification.js
@@ -1,18 +1,18 @@
 export function isAdminAuth(req, res, next) {
-    if (isAdminByHeader(req)) {
-        return next();
+    try {
+        const authheader = req.headers.authorization;
+        if (authheader) {
+            const {login, password} = JSON.parse(authheader);
+
+            if (isAdmin(login, password)) {
+                return next();
+            }
+        }
+        res.json({success: false, message: 'You are not authenticated!'});
+    } catch {
+        res.json({success: false, message: 'Authentication error!'});
     }
-    res.json({success: false, message: 'You are not authenticated!'});
 }
-
-export const isAdminByHeader = req => {
-    const authheader = req.headers.authorization;
-    if (authheader) {
-        const {login, password} = JSON.parse(authheader);
-
-        return isAdmin(login, password);
-    }
-};
 
 export const isAdmin = (login, password) => {
     return login === process.env.LOGIN && password === process.env.PASSWORD;
