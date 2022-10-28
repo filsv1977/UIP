521f13cb06fde463ad489257913ad5a0140335c0
Roms <roms10885@yandex.ru>
2022-10-28 12:40:41 +0300

    feature: add memo

--- client/src/api/fetchImplemented.js
+++ client/src/api/fetchImplemented.js
@@ -5,22 +5,22 @@ import {actionTypes} from '../сontext/actionTypes';
 axiosRetry(axios, {retries: 3});
 
 export const fetchImplemented = async (dispatch, id) => {
-  const url = `/tasks?implemented=${id}`;
+    const url = `/tasks?implemented=${id}`;
 
-  await axios
-    .get(url, {})
-    .then(result => {
-      if (!result.data.success) throw new Error(result.data.message);
+    await axios
+        .get(url, {})
+        .then(result => {
+            if (!result.data.success) throw new Error(result.data.message);
 
-      dispatch({
-        type: actionTypes.GET_IMPLEMENTED_TASKS.FULFILLED,
-        payload: {data: result.data.data, activeFilterBtn: id}
-      });
-    })
-    .catch(e =>
-      dispatch({
-        type: actionTypes.GET_IMPLEMENTED_TASKS.REJECTED,
-        payload: e.message
-      })
-    );
+            dispatch({
+                type: actionTypes.GET_IMPLEMENTED_TASKS.FULFILLED,
+                payload: {data: result.data.data, activeFilterBtn: id}
+            });
+        })
+        .catch(e =>
+            dispatch({
+                type: actionTypes.GET_IMPLEMENTED_TASKS.REJECTED,
+                payload: e.message
+            })
+        );
 };
