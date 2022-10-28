5796b86243de14497669e431dfab6c2d60a517fe
Roms <roms10885@yandex.ru>
2022-10-28 15:41:24 +0300

    feature: Mxm cache version

--- client/src/api/fetchData.js
+++ client/src/api/fetchData.js
@@ -5,11 +5,21 @@ import {getAuthorizationKey} from '../utils/localStorage';
 
 axiosRetry(axios, {retries: 3});
 
+const cache = [];
+
 export const fetchData = async (dispatch, id, noSetLoading = true, isAdmin = false) => {
     const admin = isAdmin ? '/admin' : '';
     const url = id == null ? `${admin}/tasks` : `${admin}/tasks?implemented=${id}`;
     const token = getAuthorizationKey();
     dispatch({type: actionTypes.GET_TASKS.PENDING, noSetLoading});
+
+  if (cache[id]) {
+    return dispatch({
+      type: actionTypes.GET_TASKS.FULFILLED,
+      payload: {data: cache[id], activeFilterBtn: id}
+    });
+  }
+
     await axios
         .get(
             url,
@@ -21,6 +31,8 @@ export const fetchData = async (dispatch, id, noSetLoading = true, isAdmin = fal
         )
         .then(result => {
             if (!result.data.success) throw new Error(result.data.message);
+
+          cache[id] = result.data.data;
 
             dispatch({
                 type: actionTypes.GET_TASKS.FULFILLED,
