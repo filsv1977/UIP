824f670153f324c44adb5572584e2afd0b10c991
Roms <roms10885@yandex.ru>
2022-10-31 09:59:48 +0300

    feature: without memo

--- client/src/hooks/useAssignmentActions.js
+++ client/src/hooks/useAssignmentActions.js
@@ -5,29 +5,11 @@ import {exportDB} from '../api/getDB';
 import {logOut} from '../api/logout';
 import {implementedText, openText} from '../constants';
 import {importDB} from '../api/importDB';
-import {useMemo} from 'react';
 
 export default function useAssignmentActions(isAdmin = false) {
-    const {
-        dispatch,
-        state: {activeFilterBtn}
-    } = useTasks();
+    const {dispatch} = useTasks();
     const navigate = useNavigate();
 
-    const loadData = useMemo(() => {
-        let prevButton = activeFilterBtn;
-        return async (activeButton, setActive, dispatch) => {
-            if (+activeButton === +prevButton) {
-                return;
-            }
-            prevButton = activeButton;
-            let path = activeButton ? '/implemented' : '/open';
-            fetchData(dispatch, activeButton).then(_ => {
-                navigate(path);
-                setActive(activeButton);
-            });
-        };
-    }, []);
     const userAction = {
         OPEN: 0,
         IMPLEMENTED: 1
@@ -50,7 +32,9 @@ export default function useAssignmentActions(isAdmin = false) {
                 variant: active => (+active === +userAction.OPEN ? 'outline-danger' : 'outline-primary')
             },
             onClick: (e, setActive) => {
-                loadData(userAction.OPEN, setActive, dispatch);
+                fetchData(dispatch, 0, null, true).then(_ => {
+                    setActive(userAction.OPEN);
+                });
             }
         },
         {
@@ -60,7 +44,9 @@ export default function useAssignmentActions(isAdmin = false) {
                 variant: active => (+active === +userAction.IMPLEMENTED ? 'outline-danger' : 'outline-primary')
             },
             onClick: (e, setActive) => {
-                loadData(userAction.IMPLEMENTED, setActive, dispatch);
+                fetchData(dispatch, 1, null, true).then(_ => {
+                    setActive(userAction.IMPLEMENTED);
+                });
             }
         }
     ];
