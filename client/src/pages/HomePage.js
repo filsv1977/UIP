4d9a6979598801c4f5016cf3602e22c8d4b8ca9f
Roms <roms10885@yandex.ru>
2022-11-04 10:15:39 +0300

    fix fetch data

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react';
 import Content from '../components/Content';
 import {useTasks} from '../сontext/reducer';
 import {fetchData} from '../api/fetchData';
-import {delAuthorizationKey} from '../utils/localStorage';
 
 function HomePage({implemented}) {
     const {
@@ -11,7 +10,6 @@ function HomePage({implemented}) {
     } = useTasks();
 
     useEffect(() => {
-        delAuthorizationKey();
         fetchData(dispatch, implemented);
     }, []);
 
